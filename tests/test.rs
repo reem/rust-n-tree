e80ebac9a46a6fc537524507457b547830b90845
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-13 19:46:14 -0700

    (test) Test nearby and contains.

--- tests/test.rs
+++ tests/test.rs
@@ -1,111 +1,161 @@
 extern crate ntree;
 
 use ntree::{NTree, Region};
-
-#[deriving(Clone, Show, PartialEq)]
-struct QuadTreeRegion {
-    x: f64,
-    y: f64,
-    width: f64,
-    height: f64
-}
-
-struct Vec2 {
-    x: f64, y:f64
-}
-
-impl QuadTreeRegion {
-    fn square(x: f64, y:f64, wh: f64) -> QuadTreeRegion {
-        QuadTreeRegion { x: x, y: y, width: wh, height: wh }
-    }
-}
-
-impl Region<Vec2> for QuadTreeRegion {
-    fn contains(&self, p: &Vec2) -> bool {
-        self.x < p.x && self.y < p.y && (self.x + self.width) > p.x && (self.y + self.height) > p.y
-    }
-
-    fn split(&self) -> Vec<QuadTreeRegion> {
-        let halfwidth = self.width / 2.0;
-        let halfheight = self.height / 2.0;
-        vec![
-            QuadTreeRegion {
-                x: self.x,
-                y: self.y,
-                width: halfwidth,
-                height: halfheight
-            },
-
-            QuadTreeRegion {
-                x: self.x,
-                y: self.y + halfheight,
-                width: halfwidth,
-                height: halfheight
-            },
-
-            QuadTreeRegion {
-                x: self.x + halfwidth,
-                y: self.y,
-                width: halfwidth,
-                height: halfheight
-            },
-
-            QuadTreeRegion {
-                x: self.x + halfwidth,
-                y: self.y + halfheight,
-                width: halfwidth,
-                height: halfheight
-            }
-        ]
-    }
-
-    fn overlaps(&self, other: &QuadTreeRegion) -> bool {
-        other.contains(&Vec2 { x: self.x, y: self.y })
-            || other.contains(&Vec2 { x: self.x + self.width, y: self.y })
-            || other.contains(&Vec2 { x: self.x, y: self.y + self.height })
-            || other.contains(&Vec2 { x: self.x + self.width, y: self.y + self.height })
-    }
-}
+use self::fixtures::{QuadTreeRegion, Vec2};
 
 #[test] fn test_contains() {
-    assert!(QuadTreeRegion::square(0.0, 0.0, 100.0).contains(&Vec2 { x: 50.0, y: 50.0 }));
+    let ntree = NTree::new(QuadTreeRegion::square(0.0, 0.0, 100.0), 4);
+    assert!(ntree.contains(&Vec2 { x: 50.0, y: 50.0 }));
 }
 
-#[test] fn test_overlaps() {
-    assert!(QuadTreeRegion::square(0.0, 0.0, 100.0).overlaps(&QuadTreeRegion::square(50.0, 50.0, 100.0)));
+#[test] fn test_insert() {
+    let mut ntree = NTree::new(QuadTreeRegion::square(0.0, 0.0, 100.0), 4);
+    assert!(ntree.insert(Vec2 { x: 49.0, y: 49.0 }));
+    assert_eq!(ntree.nearby(&Vec2 { x: 40.0, y: 40.0 }), Some(&[Vec2 { x: 49.0, y: 49.0 }]));
 }
 
-#[test] fn test_split() {
-    let fifty = 100.0 / 2.0;
-    assert_eq!(QuadTreeRegion::square(0.0, 0.0, 100.0).split(),
-        vec![
-            QuadTreeRegion {
-                x: 0.0,
-                y: 0.0,
-                width: fifty,
-                height: fifty
-            },
+#[test] fn test_nearby() {
+    let mut ntree = NTree::new(QuadTreeRegion::square(0.0, 0.0, 100.0), 4);
 
-            QuadTreeRegion {
-                x: 0.0,
-                y: 0.0 + fifty,
-                width: fifty,
-                height: fifty
-            },
+    // Bottom left corner
+    ntree.insert(Vec2 { x: 30.0, y: 30.0 });
+    ntree.insert(Vec2 { x: 20.0, y: 20.0 });
+    ntree.insert(Vec2 { x: 10.0, y: 10.0 });
 
-            QuadTreeRegion {
-                x: 0.0 + fifty,
-                y: 0.0,
-                width: fifty,
-                height: fifty
-            },
+    // Top right corner
+    ntree.insert(Vec2 { x: 75.0, y: 75.0 });
 
-            QuadTreeRegion {
-                x: 0.0 + fifty,
-                y: 0.0 + fifty,
-                width: fifty,
-                height: fifty
-            }
-        ]
-    )
+    // Top left corner
+    ntree.insert(Vec2 { x: 40.0, y: 70.0 });
+
+    // Bottom right corner
+    ntree.insert(Vec2 { x: 80.0, y: 20.0 });
+
+    // Bottom left corner
+    assert_eq!(ntree.nearby(&Vec2 { x: 40.0, y: 40.0 }),
+        Some(&[Vec2 { x: 30.0, y: 30.0 },
+               Vec2 { x: 20.0, y: 20.0 },
+               Vec2 { x: 10.0, y: 10.0 }]));
+
+    // Top right corner
+    assert_eq!(ntree.nearby(&Vec2 { x: 90.0, y: 90.0 }), Some(&[Vec2 { x: 75.0, y: 75.0 }]));
+
+    // Top left corner
+    assert_eq!(ntree.nearby(&Vec2 { x: 20.0, y: 80.0 }), Some(&[Vec2 { x: 40.0, y: 70.0 }]));
+
+    // Bottom right corner
+    assert_eq!(ntree.nearby(&Vec2 { x: 94.0, y: 12.0 }), Some(&[Vec2 { x: 80.0, y: 20.0 }]));
+}
+
+mod fixtures {
+    use ntree::Region;
+
+    #[deriving(Clone, Show, PartialEq)]
+    pub struct QuadTreeRegion {
+        x: f64,
+        y: f64,
+        width: f64,
+        height: f64
+    }
+
+    #[deriving(Show, PartialEq, Clone)]
+    pub struct Vec2 {
+        pub x: f64, pub y:f64
+    }
+
+    impl QuadTreeRegion {
+        pub fn square(x: f64, y:f64, wh: f64) -> QuadTreeRegion {
+            QuadTreeRegion { x: x, y: y, width: wh, height: wh }
+        }
+    }
+
+    impl Region<Vec2> for QuadTreeRegion {
+        fn contains(&self, p: &Vec2) -> bool {
+            self.x < p.x && self.y < p.y && (self.x + self.width) > p.x && (self.y + self.height) > p.y
+        }
+
+        fn split(&self) -> Vec<QuadTreeRegion> {
+            let halfwidth = self.width / 2.0;
+            let halfheight = self.height / 2.0;
+            vec![
+                QuadTreeRegion {
+                    x: self.x,
+                    y: self.y,
+                    width: halfwidth,
+                    height: halfheight
+                },
+
+                QuadTreeRegion {
+                    x: self.x,
+                    y: self.y + halfheight,
+                    width: halfwidth,
+                    height: halfheight
+                },
+
+                QuadTreeRegion {
+                    x: self.x + halfwidth,
+                    y: self.y,
+                    width: halfwidth,
+                    height: halfheight
+                },
+
+                QuadTreeRegion {
+                    x: self.x + halfwidth,
+                    y: self.y + halfheight,
+                    width: halfwidth,
+                    height: halfheight
+                }
+            ]
+        }
+
+        fn overlaps(&self, other: &QuadTreeRegion) -> bool {
+            other.contains(&Vec2 { x: self.x, y: self.y })
+                || other.contains(&Vec2 { x: self.x + self.width, y: self.y })
+                || other.contains(&Vec2 { x: self.x, y: self.y + self.height })
+                || other.contains(&Vec2 { x: self.x + self.width, y: self.y + self.height })
+        }
+    }
+
+    #[test] fn test_contains() {
+        assert!(QuadTreeRegion::square(0.0, 0.0, 100.0).contains(&Vec2 { x: 50.0, y: 50.0 }));
+    }
+
+    #[test] fn test_overlaps() {
+        assert!(QuadTreeRegion::square(0.0, 0.0, 100.0).overlaps(&QuadTreeRegion::square(50.0, 50.0, 100.0)));
+    }
+
+    #[test] fn test_split() {
+        let fifty = 100.0 / 2.0;
+        assert_eq!(QuadTreeRegion::square(0.0, 0.0, 100.0).split(),
+            vec![
+                QuadTreeRegion {
+                    x: 0.0,
+                    y: 0.0,
+                    width: fifty,
+                    height: fifty
+                },
+
+                QuadTreeRegion {
+                    x: 0.0,
+                    y: 0.0 + fifty,
+                    width: fifty,
+                    height: fifty
+                },
+
+                QuadTreeRegion {
+                    x: 0.0 + fifty,
+                    y: 0.0,
+                    width: fifty,
+                    height: fifty
+                },
+
+                QuadTreeRegion {
+                    x: 0.0 + fifty,
+                    y: 0.0 + fifty,
+                    width: fifty,
+                    height: fifty
+                }
+            ]
+        )
+    }
 }
